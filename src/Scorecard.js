2053eed0ae8b1f3580161e449434ceb3dab45483
Mike DeAngelis <mikedeangelis@MikeMacbookPro.local>
2022-07-08 16:09:10 -0400

    Loading animation for scorecard page

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link, useParams } from "react-router-dom";
-import { TournamentContext } from "./App";
-import { Header } from "./Header";
-import { Loading } from "./Loading";
+import React, { useState, useEffect, useContext } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { TournamentContext } from './App';
+import { Header } from './Header';
+import { Loading } from './Loading';
+import { useSpring, useTransition, animated } from 'react-spring';
 
-import EnsureAnimation from "ensure-animation";
+// import EnsureAnimation from 'ensure-animation';
 
 /* todo
  * Figure out how to store hole/course data globally (figuring out par on each hole)
@@ -14,6 +15,8 @@ function Scorecard() {
   const [player, setPlayer] = useState({});
   const [currentHole, setCurrentHole] = useState(null);
   const [newScores, setNewScores] = useState([0, 0]);
+  const [isDoneLoading, setIsDoneLoading] = useState(false);
+  const [logoShouldStop, setLogoShouldStop] = useState(false);
   const tournament = useContext(TournamentContext);
   let preloader;
 
@@ -25,11 +28,11 @@ function Scorecard() {
 
   const validateScores = (callback, nextHole) => {
     const hasEmptyScore = newScores.filter(
-      (score) => score === 0 || score === null || score === ""
+      (score) => score === 0 || score === null || score === ''
     ).length;
     if (hasEmptyScore) {
       const text =
-        "You have an empty score. Are you sure you want to go to next hole?";
+        'You have an empty score. Are you sure you want to go to next hole?';
       if (window.confirm(text) === true) {
         callback(nextHole);
       } else {
@@ -41,16 +44,16 @@ function Scorecard() {
   };
 
   const handleScoreOnBlur = (newScore, i = null) => {
-    if (newScore === "" || newScore === 0) {
+    if (newScore === '' || newScore === 0) {
       handleSetNewScores(0, i);
     }
   };
 
   const handleScoreOnFocus = (newScore, i = null) => {
-    console.log("handleScoreOnFocus", newScore);
+    console.log('handleScoreOnFocus', newScore);
     if (newScore == 0) {
-      console.log("zero score");
-      handleSetNewScores("", i);
+      console.log('zero score');
+      handleSetNewScores('', i);
     }
   };
 
@@ -60,11 +63,11 @@ function Scorecard() {
       setNewScores(newScore);
       return;
     }
-    if (newScore !== "" && newScore !== null) {
+    if (newScore !== '' && newScore !== null) {
       newScore = Number(newScore);
     }
-    console.log("newScore updated", newScore);
-    console.log("old scores.", newScores);
+    console.log('newScore updated', newScore);
+    console.log('old scores.', newScores);
     setNewScores(newScores.map((score, j) => (j === i ? newScore : score)));
   };
 
@@ -73,18 +76,19 @@ function Scorecard() {
       `${process.env.REACT_APP_API_URL}/players/details/${id}`
     );
     const newPlayer = await res.json();
-    preloader.finish().then(() => {
-      setPlayer(newPlayer);
-      setCurrentHole(newPlayer.currentHole);
-    });
+    // preloader.finish().then(() => {
+    setPlayer(newPlayer);
+    setCurrentHole(newPlayer.currentHole);
+    setIsDoneLoading(true);
+    // });
     // setPlayer(newPlayer);
     // setCurrentHole(newPlayer.currentHole);
   };
 
   const updateCurrentHole = async (newHole) => {
     const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ id: player._id, currentHole: newHole }),
     };
     const response = await fetch(
@@ -96,10 +100,10 @@ function Scorecard() {
   };
 
   const updatePlayerScore = async (newScores, par) => {
-    console.log("updatePlayerScore()");
+    console.log('updatePlayerScore()');
     const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ id: player._id, currentHole, newScores, par }),
     };
     const response = await fetch(
@@ -111,12 +115,12 @@ function Scorecard() {
   };
 
   useEffect(() => {
-    preloader = new EnsureAnimation(".app-logo")[0]; // get our first instance
+    // preloader = new EnsureAnimation('.app-logo')[0]; // get our first instance
     getPlayerDetails(id);
   }, [id]);
 
   useEffect(() => {
-    console.log("currentHole updated.", currentHole);
+    console.log('currentHole updated.', currentHole);
     if (currentHole) {
       handleSetNewScores(player?.scorecard[currentHole - 1]?.scores);
       updateCurrentHole(currentHole);
@@ -124,98 +128,122 @@ function Scorecard() {
   }, [currentHole]);
 
   useEffect(() => {
-    console.log("newScores changed", newScores);
+    console.log('newScores changed', newScores);
     const isEmptyScore = newScores.every((item) => item === 0);
     const hasNullScore = newScores.filter(
-      (item) => item === null || item === ""
+      (item) => item === null || item === ''
     ).length;
-    console.log("isEmptyScore", isEmptyScore);
-    console.log("hasNullScore", hasNullScore);
+    console.log('isEmptyScore', isEmptyScore);
+    console.log('hasNullScore', hasNullScore);
     if (!isEmptyScore && !hasNullScore) {
-      console.log("Updated score.");
+      console.log('Updated score.');
       updatePlayerScore(newScores, tournament?.holes[currentHole - 1].par);
     } else {
-      console.log("Empty score.");
+      console.log('Empty score.');
     }
   }, [newScores]);
 
-  if (tournament?.name && player?.name) {
-    return (
-      <div>
+  const logoStyles = useSpring({
+    loop: true,
+    from: { rotateZ: 0 },
+    to: { rotateZ: 360 },
+    cancel: logoShouldStop,
+    onRest: () => setLogoShouldStop(isDoneLoading),
+  });
+
+  const transitions = useTransition(isDoneLoading, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    delay: 200,
+  });
+
+  // if (tournament?.name && player?.name) {
+  return (
+    <div>
+      <animated.div style={logoStyles}>
         <Header />
-        <div className="scorecard-header">
-          <button
-            className="nav-btn"
-            onClick={() => setCurrentHole(getPrevHole(currentHole))}
-          >
-            &lt; Hole {getPrevHole(currentHole)}
-          </button>
-          <div className="hole-info">
-            <p className="hole-number">Hole {currentHole}</p>
-            <p className="hole-par">
-              Par {tournament?.holes[currentHole - 1]?.par} &#8226;{" "}
-              {tournament?.holes[currentHole - 1]?.yards} yards
-            </p>
-          </div>
-          <button
-            className="nav-btn"
-            // onClick={() => setCurrentHole(getNextHole(currentHole))}
-            onClick={() =>
-              validateScores(setCurrentHole, getNextHole(currentHole))
-            }
-          >
-            Hole {getNextHole(currentHole)} &gt;
-          </button>
-        </div>
-        {newScores?.length && (
-          <div className="scorecard">
-            <div className="scorecard-player-details">
-              <div className="scorecard-team">
-                <p className="scorecard-team-name">
-                  {player.name} ({player.handicap})
-                </p>
-                <p className="scorecard-team-score">
-                  {player.parScore > 0 && <span>+</span>}
-                  {player.parScore}
-                </p>
+      </animated.div>
+      {transitions(
+        (styles, item) =>
+          item && (
+            <div>
+              <div className="scorecard-header">
+                <button
+                  className="nav-btn"
+                  onClick={() => setCurrentHole(getPrevHole(currentHole))}
+                >
+                  &lt; Hole {getPrevHole(currentHole)}
+                </button>
+                <div className="hole-info">
+                  <p className="hole-number">Hole {currentHole}</p>
+                  <p className="hole-par">
+                    Par {tournament?.holes[currentHole - 1]?.par} &#8226;{' '}
+                    {tournament?.holes[currentHole - 1]?.yards} yards
+                  </p>
+                </div>
+                <button
+                  className="nav-btn"
+                  // onClick={() => setCurrentHole(getNextHole(currentHole))}
+                  onClick={() =>
+                    validateScores(setCurrentHole, getNextHole(currentHole))
+                  }
+                >
+                  Hole {getNextHole(currentHole)} &gt;
+                </button>
               </div>
-              <div className="scorecard-players">
-                {player.players.map((teamPlayer, i) => (
-                  <div className="scorecard-player" key={`player${i}`}>
-                    <p className="scorecard-player-name">{teamPlayer.name}</p>
-                    <div className="scorecard-input">
-                      <input
-                        type="text"
-                        maxLength={2}
-                        inputMode="numeric"
-                        onFocus={(event) =>
-                          handleScoreOnFocus(event.target.value, i)
-                        }
-                        onBlur={(event) =>
-                          handleScoreOnBlur(event.target.value, i)
-                        }
-                        value={newScores[i]}
-                        onChange={(event) =>
-                          handleSetNewScores(event.target.value, i)
-                        }
-                      />
+              {newScores?.length && (
+                <div className="scorecard">
+                  <div className="scorecard-player-details">
+                    <div className="scorecard-team">
+                      <p className="scorecard-team-name">
+                        {player.name} ({player.handicap})
+                      </p>
+                      <p className="scorecard-team-score">
+                        {player.parScore > 0 && <span>+</span>}
+                        {player.parScore}
+                      </p>
+                    </div>
+                    <div className="scorecard-players">
+                      {player.players.map((teamPlayer, i) => (
+                        <div className="scorecard-player" key={`player${i}`}>
+                          <p className="scorecard-player-name">
+                            {teamPlayer.name}
+                          </p>
+                          <div className="scorecard-input">
+                            <input
+                              type="text"
+                              maxLength={2}
+                              inputMode="numeric"
+                              onFocus={(event) =>
+                                handleScoreOnFocus(event.target.value, i)
+                              }
+                              onBlur={(event) =>
+                                handleScoreOnBlur(event.target.value, i)
+                              }
+                              value={newScores[i]}
+                              onChange={(event) =>
+                                handleSetNewScores(event.target.value, i)
+                              }
+                            />
+                          </div>
+                        </div>
+                      ))}
                     </div>
                   </div>
-                ))}
-              </div>
+                </div>
+              )}
+              <hr />
+              <Link to="/">Back to home</Link>
+              <br />
+              <br />
+              <Link to="/leaderboard">View full leaderboard</Link>
             </div>
-          </div>
-        )}
-        <hr />
-        <Link to="/">Back to home</Link>
-        <br />
-        <br />
-        <Link to="/leaderboard">View full leaderboard</Link>
-      </div>
-    );
-  } else {
-    return <Loading />;
-  }
+          )
+      )}
+    </div>
+  );
+  // }
 }
 
 export default Scorecard;
